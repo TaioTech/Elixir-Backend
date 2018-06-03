b40b3f5eb3a4ac4e2479b645262aad90a63a39e4
Admin <tao@admins-mbp.attlocal.net>
2018-06-03 05:42:29 -0500

    Initial commit

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :project_manager_example, ProjectManagerExample.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "tyler",
+  password: "pawargsk",
   database: "project_manager_example_dev",
   hostname: "localhost",
   pool_size: 10
